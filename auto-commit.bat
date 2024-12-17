37b0e45be128eb787716f67a154191dafc6828c3
nmissi-nadia <nmissinadia@gmail.com>
2024-12-17 17:20:52 +0100

    Modification de cot├® responsive

--- auto-commit.bat
+++ auto-commit.bat
@@ -8,7 +8,7 @@ cd /d "C:\laragon\www\AvocatConnect"
 git add .
 
 :: Commit avec un message automatique
-git commit -m "Ajout de coté responsive"
+git commit -m "Modification de coté responsive"
 
 :: Pousser les modifications sur la branche principale (main ou master)
 git push origin main
